e6b4fd26081c98bfbf5197c0fb016c7f66e44790
yamanokuchi <kouheiyamanokuchi@gmail.com>
2025-07-31 16:36:17 +0900

    ♻️ Footer部分とLinkの部分をコンポーネント化

---
+++ components/Links.js
@@ -0,0 +1,35 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Geist, Geist_Mono } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import Footer from "@/components/Footer";
+
+export default function Home() {
+  return (
+    <div className={styles.ctas}>
+      <a
+        className={styles.primary}
+        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Image
+          className={styles.logo}
+          src="/vercel.svg"
+          alt="Vercel logomark"
+          width={20}
+          height={20}
+        />
+        Deploy now
+      </a>
+      <a
+        href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+        className={styles.secondary}
+      >
+        Read our docs
+      </a>
+    </div>
+  );
+}
